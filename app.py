a268183556b1cdecca8356e3812ebc3c96d62523
ckareez <Greekrush6!>
2021-10-19 19:09:47 -0400

    fixed spaces

--- app.py
+++ app.py
@@ -3,22 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 # DEFAULT
-
-
 @app.route('/', methods=['GET'])
 def index():
     return 'Mike and Constantines Cloud Computing Attempt with Heroku'
 
 # Returns what is requested based using JSON
-
-
 @app.route('/api/v1/json/batches', methods=['GET'])
 def get_data():
     return 'data'
 
 # Returns what is requested using a Protocol Buffer
-
-
 @app.route('/api/v1/protobuf/batches', methods=['GET'])
 def get_protobuf():
     return 'protobuf data'
