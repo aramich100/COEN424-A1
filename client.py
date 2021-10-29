843c1624f5d52f9e460b33afff73f447f009baea
ckareez <Greekrush6!>
2021-10-29 12:44:29 -0400

    client updated

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-# Protobuff Imports
+#protobuff imports made from our server app.py
 import request_pb2
 import response_pb2
 
@@ -6,92 +6,86 @@ import string
 import requests
 import random
 
+#heroku server URL made by running the server on heroku deployment
+baseURL = "https://coen424-assignmnent1-heroku.herokuapp.com"
 
-# Heroku Server Base URL
-base_url = "https://coen424-assignmnent1-heroku.herokuapp.com"
+#JSON URL to access the json extension of the server
+JSONURL = baseURL+"/api/v1/json/batches"
 
-# JSON URL Extension
-url_json = base_url+"/api/v1/json/batches"
+#protobuf URL to access the protobuff extension of the server
+ProtoURL = baseURL+"/api/v1/protobuf/batches"
 
-# Protobuf URL Extension
-url_protobuf = base_url+"/api/v1/protobuf/batches"
-
-
-# Generates a random string (as we need to make this unique)
+#generates a random ascii string to identify the unique samples
 def genRandom():
     l = string.ascii_lowercase
-    return ''.join(random.choice(l) for i in range(10))
+    return ''.join(random.choice(l) for j in range(10))
 
-
-# This function is used to call the server with json protocol
+#requesting json from the server
 def reqJSON(benchmarkType, workloadMetric, batchUnit, batchID, batchSize):
+    #creating a json object with our variables
+    jsonObject= {"rfwID": genRandom(), "benchmarkType": benchmarkType,
+                 "workloadMetric": workloadMetric, "batchUnit": int(batchUnit),
+                 "batchID": int(batchID), "batchSize": int(batchSize)}
 
-    json_obj = {
-        "rfw_id": genRandom(),
-        "benchmark_type": benchmarkType,
-        "workload_metric": workloadMetric,
-        "batch_unit": int(batchUnit),
-        "batch_id": int(batchID),
-        "batch_size": int(batchSize)}
-
-    # Send request to server
-    response = requests.get(url_json, json=json_obj)
+    #making a response to the server
+    response = requests.get(JSONURL, json=jsonObject)
 
     if (response.status_code > 200):
-        print(" -- ERROR CODE -- :  ", response.status_code)
+        print("ERROR CODE: ", response.status_code)
 
-    # Print result
-    json_resp = response.json()
-    if json_resp.get('rfw_id'):
-        print(' Request ID: '+str(json_resp.get('rfw_id')))
-        print(' Last batch ID: '+str(json_resp.get('last_batch_id')))
-        print(' Samples: '+str(json_resp.get('samples')))
+    #displaying the json response 
+    jsonResp = response.json()
+    if jsonResp.get('rfwID'):
+        print('Request ID: '+str(jsonResp.get('rfwID')))
+        print('Last batch ID: '+str(jsonResp.get('lastBatchID')))
+        print('Samples: '+str(jsonResp.get('Samples')))
     else:
-        print(json_resp)
+        print(jsonResp)
 
-
-# This function is used to call the server with protcol buffer
+#requesting protobuff (XML) from the server
 def reqProtobuf(benchmarkType, workloadMetric, batchUnit, batchID, batchSize):
-    # Create protocol buffer object
-    request_RFW_protobuf = request_pb2.RFW()
-    request_RFW_protobuf.rfw_id = genRandom()
-    request_RFW_protobuf.benchmark_type = benchmarkType
-    request_RFW_protobuf.workload_metric = workloadMetric
-    request_RFW_protobuf.batch_unit = batchUnit
-    request_RFW_protobuf.batch_id = batchID
-    request_RFW_protobuf.batch_size = batchSize
+    #protobuf object
+    requProtoRFW = request_pb2.RFW()
+    requProtoRFW.rfw_id = genRandom()
+    requProtoRFW.benchmark_type = benchmarkType
+    requProtoRFW.workload_metric = workloadMetric
+    requProtoRFW.batch_unit = batchUnit
+    requProtoRFW.batch_id = batchID
+    requProtoRFW.batch_size = batchSize
 
-    # Make the request to the server
-    response = requests.get(url_protobuf,
-                            data=request_RFW_protobuf.SerializeToString(),
-                            headers={'Content-Type': 'application/octet-stream'})
+    #requesting to the server
+    response = requests.get(ProtoURL, data=requProtoRFW.SerializeToString(), headers = {'Content-Type': 'application/octet-stream'})
 
-    # Make sure the response is successful
+    #validating the response
     if (response.status_code > 200):
-        print("Something went wrong the response may be bad!")
+        print("ERROR: There may have been a problem with the response.")
         print(response.content)
     else:
-        # Process successful response
-        response_protobuf_RFD = response_pb2.RFD()
-        response_protobuf_RFD.ParseFromString(response.content)
-        print(response_protobuf_RFD)
+        #this is a good response
+        respProtoRFD = response_pb2.RFD()
+        respProtoRFD.ParseFromString(response.content)
+        print(respProtoRFD)
 
+#user input for client to run application
+#user can access the json or protobuf
+objectType = input("Enter 'JSON' or 'ProtoBuf': ")
+#user can choose which csv file to enter
+benchmarkType = input("Enter BenchMark Type  (DVD-testing, DVD-training, NDBench-testing, NDBench-training): ")
+#user chooses the column of the csv file 
+workloadMetric = input("Enter Workload Metric (CPUUtilization_Average, NetworkIn_Average ,NetworkOut_Average, MemoryUtilization_Average, Final_Target: ")
+#user enters the batch unit
+batchUnit = input("Enter Batch Unit (0,1,2...): ")
+#user enters the batch id
+batchID = input("Enter batch ID (0,1,2...): ")
+#user enters the batch size
+batchSize = input("Enter batch size (0,1,2...): ")
 
-# USER INPUT
-objectType = input("Enter 'JSON' or 'ProtoBuff':   ")
-
-benchmarkType = input(
-    "Enter BenchMark Type  (DVD-testing, DVD-training, NDBench-testing, NDBench-training)  : ")
-workloadMetric = input(
-    "Enter Workload Metric (CPUUtilization_Average, NetworkIn_Average ,NetworkOut_Average, MemoryUtilization_Average, Final_Target :    ")
-batchUnit = input("Enter Batch Unit (0,1,2...)    :  ")
-batchID = input("Enter batch ID     (0,1,2...)    :  ")
-batchSize = input("Enter batch size (0,1,2...)    :  ")
-
+#if the user chose JSON, then request json
 if objectType == 'JSON':
     reqJSON(benchmarkType, workloadMetric,
             int(batchUnit), int(batchID), int(batchSize))
 
-if objectType == 'ProtoBuff':
+#if user chose protobuf, then request protobuf
+if objectType == 'ProtoBuf':
     reqProtobuf(benchmarkType, workloadMetric,
                 int(batchUnit), int(batchID), int(batchSize))
